871059b44425542cf9c7d8820e710d03179bd818
eunbee0 <lovehawaiigangster@gmail.com>
2022-06-24 18:20:01 +0900

    project

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
 $('header').load('inc.html header > div');
 
 $('footer').load('inc.html footer > div');
+
+$(function () {
+   $('.slide').slick();
+});
+
+$(function () {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 800) {
+         $('#MOVE_TOP_BTN').fadeIn();
+      } else {
+         $('#MOVE_TOP_BTN').fadeOut();
+      }
+   });
+
+   $("#MOVE_TOP_BTN").click(function () {
+      $('html, body').animate({
+         scrollTop: 0
+      }, 500);
+      return false;
+   });
+});
+
+AOS.init(); 
